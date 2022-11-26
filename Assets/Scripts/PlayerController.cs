9afc217d5a958fa6f136f0f054a8547439305762
Yuri Campos <ygsoares20@gmail.com>
2022-11-26 04:12:36 -0300

    Write playerController movement script

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,15 +4,29 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public float playerVel;
+    private Vector2 keyInput;
+    public Rigidbody2D playerRigidBody;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        movePlayer();
+    }
+
+    private void movePlayer()
+    {
+        keyInput = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+
+        Vector3 pMovementHorizontal = transform.up * -keyInput.x;
+        Vector3 pMovementVertical = transform.right * keyInput.y;
+
+        playerRigidBody.velocity = (pMovementHorizontal + pMovementVertical) * playerVel;
     }
 }
