5babaf03f9cf8de5a663605edacbe1bd7f8f7788
Yuri Campos <ygsoares20@gmail.com>
2022-11-28 00:45:15 -0300

    implement a first idea for the player attack and ammo count

---
+++ Assets/Scripts/PlayerAttack.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAttack : MonoBehaviour
+{
+    public Camera gameCam;
+    public int maxAmmo;
+    public int ammo;
+    public int reserveAmmo;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        shoot();
+        reload();
+    }
+
+
+    private void shoot()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            if(ammo == 0)
+            {
+                Debug.Log("Press TEMP to reload");
+                return;
+            }
+            Ray hitscanRay = gameCam.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0f));
+            RaycastHit hitLocation;
+            ammo--;
+            if (Physics.Raycast(hitscanRay, out hitLocation))
+            {
+                Debug.Log("Hit: " + hitLocation.transform.name);
+            }
+            else
+            {
+                Debug.Log("Miss");
+            }
+        }
+    }
+
+    private void reload()
+    {
+        if(Input.GetButtonDown("Fire2") && maxAmmo > 0)
+        {
+            reserveAmmo -= maxAmmo - ammo;
+            ammo = maxAmmo - ammo;
+            Debug.Log(reserveAmmo);     
+        }   
+    }
+}
